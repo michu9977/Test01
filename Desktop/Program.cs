2d4b7f9b9e854c6e31c57b4cadc89fff8361b918
michu9977 <jacekpietko@wp.pl>
2019-11-06 19:16:58 +0100

    asdasd

---
+++ Desktop/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("podaj wzrost i mase");
+            double masa = double.Parse(Console.ReadLine());
+
+            double wzrost = double.Parse(Console.ReadLine());
+
+            Double bmi = wzrost/(masa * masa);
+            Console.WriteLine(bmi);
+
+
+            pracownik pracowniczek = new pracownik();
+            
+            Console.ReadKey();
+    }
+    }
+    public class pracownik
+    {
+        public char imie;
+        public char nazwisko;
+        public double pensja;
+        void RoczneZarobki(int pensja)
+        {
+            Console.WriteLine("Roczna pensja pracownika wynosi" + (pensja * 12));
+        }
+    }
+    
+   
+}
